7ef5273312c0f25b74d23b802661197a93f902b4
unknown <eddison>
2022-04-27 20:52:08 -0500

    cambio de react

---
+++ 2.reactcore/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const App = (props)=>{
+  return <h1>{props.name}   componente de app</h1>
+
+};
+
+export default App;
---
+++ 2.reactcore/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./App"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <App name="Avatar" status={1}/>
+);
+
+
+// <App name="Avatar" status={1}/> esto es js con html recuerde para recoger 
+// solo js pones {}
+
+
